38dfc232b15a7cd6bb115d3e14691766e088dc71
ishika2203 <ishikabatra.net@gmail.com>
2024-01-03 01:51:42 +0530

    nesting components

--- src/index.js
+++ src/index.js
@@ -1,39 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+// JSX SYNTAX
 // function Greetings() {
-//   return <h2>My First Component</h2>;
-// }
-
-// function Greetings() {
-//   return React.createElement('h2', {}, 'Hello World!');
-// }
-
-function Greetings() {
-  return (
-    <>
-      <div>
-        <h2>Hello World!</h2>
-      </div>
-      <ul className="list">
-        <li>pink</li>
-      </ul>
-      <img
-        src="https://cdn11.bigcommerce.com/s-3uewkq06zr/images/stencil/400x400/products/167/545/fluorescent_pink__88286.1492541116.png?c=2"
-        alt=""
-      />
-    </>
-  );
-}
-
-// function Greetings() {
-//   return React.createElement(
-//     'div',
-//     {},
-//     React.createElement('h2', {}, 'Hello world!')
+//   return (
+//     <>
+//       <div>
+//         <h2>Hello World!</h2>
+//       </div>
+//       <ul className="list">
+//         <li>pink</li>
+//       </ul>
+//       <img
+//         src="https://cdn11.bigcommerce.com/s-3uewkq06zr/images/stencil/400x400/products/167/545/fluorescent_pink__88286.1492541116.png?c=2"
+//         alt=""
+//       />
+//     </>
 //   );
 // }
 
+// NESTING COMPONENTS
+
+function Greetings() {
+  return (
+    <div>
+      <Person />
+      <Message />
+    </div>
+  );
+}
+
+const Person = () => <h2>Ishika Batra</h2>;
+const Message = () => (
+  <>
+    <p>This is my Message</p>
+    <p>yo guys</p>
+  </>
+);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(<Greetings />);
