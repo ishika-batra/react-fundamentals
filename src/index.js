89c423a041665ae4868305902b3ac8994094a553
ishika2203 <ishikabatra.net@gmail.com>
2024-01-04 00:15:16 +0530

    booklist template added

--- src/index.js
+++ src/index.js
@@ -1,41 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// JSX SYNTAX
-// function Greetings() {
-//   return (
-//     <>
-//       <div>
-//         <h2>Hello World!</h2>
-//       </div>
-//       <ul className="list">
-//         <li>pink</li>
-//       </ul>
-//       <img
-//         src="https://cdn11.bigcommerce.com/s-3uewkq06zr/images/stencil/400x400/products/167/545/fluorescent_pink__88286.1492541116.png?c=2"
-//         alt=""
-//       />
-//     </>
-//   );
-// }
 
-// NESTING COMPONENTS
-
-function Greetings() {
+const BookList = () => {
   return (
-    <div>
-      <Person />
-      <Message />
-    </div>
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
-}
+};
 
-const Person = () => <h2>Ishika Batra</h2>;
-const Message = () => (
-  <>
-    <p>This is my Message</p>
-    <p>yo guys</p>
-  </>
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+const Image = () => (
+  <img src="https://m.media-amazon.com/images/I/71CvcFBz4pL._AC_UY327_FMwebp_QL65_.jpg" />
 );
+const Title = () => <h2>Ikigai</h2>;
+const Author = () => {
+  return <h4> Keira Miki</h4>;
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<Greetings />);
+root.render(<BookList />);
