b2bb266296e0a369d11ff88b260d95368e64b048
ishika2203 <ishikabatra.net@gmail.com>
2024-01-06 16:12:40 +0530

    list added

--- src/index.js
+++ src/index.js
@@ -3,55 +3,43 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 
-const firstBook = {
-  author: 'Keira Miki',
-  title: 'Ikigai',
-  img: './images/book1.jpg',
-};
-
-const secondBook = {
-  author: 'George S. Clason',
-  title: 'The Richest Man In Babylon',
-  img: 'https://m.media-amazon.com/images/I/71Vs-VNGhXL._AC_UY327_FMwebp_QL65_.jpg',
-};
+const books = [
+  {
+    author: 'Keira Miki',
+    title: 'Ikigai',
+    img: './images/book1.jpg',
+  },
+  {
+    author: 'George S. Clason',
+    title: 'The Richest Man In Babylon',
+    img: 'https://m.media-amazon.com/images/I/71Vs-VNGhXL._AC_UY327_FMwebp_QL65_.jpg',
+  },
+];
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
-          maxime tenetur consequuntur eius cupiditate beatae voluptate magni
-          ipsam et animi!
-        </p>
-        <button>Click Me</button>
-      </Book>
-
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
+      {books.map((book) => {
+        const { img, title, author } = book;
+        return <Book img={img} title={title} author={author} />;
+      })}
     </section>
   );
 };
 
 const Book = (props) => {
   console.log(props);
-  const { img, title, author, children } = props;
+  const { img, title, author } = props;
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 };
+
+console.log(BookList);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
