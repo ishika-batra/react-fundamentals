7d0ff9929e0042e646be01b9e95d777d1e68c999
ishika2203 <ishikabatra.net@gmail.com>
2024-01-06 20:43:08 +0530

    event handling basics added

--- src/index.js
+++ src/index.js
@@ -21,10 +21,35 @@ const books = [
 const BookList = () => {
   return (
     <section className="booklist">
+      <EventExamples />
       {books.map((book) => {
         // return <Book potato={book} key={book.id} />;
         return <Book {...book} key={book.id} />;
       })}
+    </section>
+  );
+};
+
+const EventExamples = () => {
+  const handleFormInput = () => {
+    console.log('handle form input');
+  };
+
+  const handleButtonClick = () => {
+    alert('handle button click');
+  };
+  return (
+    <section>
+      <form>
+        <h2>Typical Form</h2>
+        <input
+          type="text"
+          name="example"
+          onChange={handleFormInput}
+          style={{ margin: '1rem 0' }}
+        />
+      </form>
+      <button onClick={handleButtonClick}>Click me</button>
     </section>
   );
 };
