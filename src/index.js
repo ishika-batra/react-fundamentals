ca5e54f773fb0882883527c0335877ffc9ba4010
ishika2203 <ishikabatra.net@gmail.com>
2024-01-07 23:30:05 +0530

    props drilling added

--- src/index.js
+++ src/index.js
@@ -19,26 +19,28 @@ const books = [
 ];
 
 const BookList = () => {
+  const someValue = 'ishika';
+  const displayValue = () => {
+    console.log(someValue);
+  };
   return (
     <section className="booklist">
       {books.map((book) => {
         // return <Book potato={book} key={book.id} />;
-        return <Book {...book} key={book.id} />;
+        return <Book {...book} key={book.id} displayValue={displayValue} />;
       })}
     </section>
   );
 };
 
 const Book = (props) => {
-  const { img, title, author } = props;
-  const displayTitle = () => {
-    console.log(title);
-  };
+  const { img, title, author, displayValue } = props;
+
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
-      <button onClick={displayTitle}>display title</button>
+      <button onClick={displayValue}>Click me</button>
       <h4>{author}</h4>
     </article>
   );
