1d6602faf043a835ffa2c4db6332ef8d5df478b4
ishika2203 <ishikabatra.net@gmail.com>
2024-01-08 00:10:51 +0530

    get book function added

--- src/index.js
+++ src/index.js
@@ -19,28 +19,33 @@ const books = [
 ];
 
 const BookList = () => {
-  const someValue = 'ishika';
-  const displayValue = () => {
-    console.log(someValue);
+  const getBook = (id) => {
+    const book = books.find((book) => book.id === id);
+    console.log(book);
   };
+
   return (
     <section className="booklist">
       {books.map((book) => {
         // return <Book potato={book} key={book.id} />;
-        return <Book {...book} key={book.id} displayValue={displayValue} />;
+        return <Book {...book} key={book.id} getBook={getBook} />;
       })}
     </section>
   );
 };
 
 const Book = (props) => {
-  const { img, title, author, displayValue } = props;
+  const { img, title, author, getBook, id } = props;
+  // console.log(props);
+  const getSingleBook = () => {
+    getBook(id);
+  };
 
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
-      <button onClick={displayValue}>Click me</button>
+      <button onClick={getSingleBook}>Click me</button>
       <h4>{author}</h4>
     </article>
   );
